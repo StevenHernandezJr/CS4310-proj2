1a0dc95071f2f5f52a931585a7edbd7d986c6e25
Steven Hernandez Jr <s_h267@txstate.edu>
2019-11-19 14:33:34 -0600

    Rewrote c2c.cpp to add log functionality and to be cleaner

--- c2c.cpp
+++ c2c.cpp
@@ -1,22 +1,13 @@
-/**************************
- * Name: Steven Hernandez
- * CS 4310
- * Project 2
- * Due Date: 12-4-2019
- * ************************/
-
-// List of includes
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstring>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <iostream>
-#include <unistd.h>
-#include <string.h>
-#include <cstring>
-#include <cstdlib>
-#include <ctime>
 
 #define MAXBUF 1024
 
@@ -32,23 +23,30 @@ string getCurrentTime() {
    return s;
 }
 
+void writeFile(string s) {
+    ofstream logFile;
+    logFile.open("log.txt", ios::app);
+    logFile << s << "\n";
+    logFile.close();
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         cout << "Usage: c2c server_port" << endl;
         return 0;
     }
 
-    time_t current_time;
-    char timeBuf[256];
-
+    // Get port number
     string sPort = argv[1];
-    int nPort = atoi(sPort.c_str());
+    int port = atoi(sPort.c_str());
+
+    char buffer[MAXBUF];
+    string response, timeStr, logStr;
 
     struct sockaddr_in my_addr;
-    struct sockaddr_in agent_addr;
-    int addrlen = sizeof(agent_addr);
+    struct sockaddr_in client_addr;
+    int addrlen = sizeof(client_addr);
 
-    // Create a socket to use for communication
     int sd, new_sd;
     if ((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
         cerr << "ERROR: socket() failed" << endl;
@@ -57,7 +55,7 @@ int main(int argc, char* argv[]) {
     cout << "Socket Created" << endl;
 
     my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(nPort);
+    my_addr.sin_port = htons(port);
     my_addr.sin_addr.s_addr = INADDR_ANY;
 
     // Bind the socket to IP address and port #
@@ -72,38 +70,38 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    char buffer[MAXBUF];
-    string response;
-
     while(1) {
-        if ((new_sd = accept(sd, (struct sockaddr *)&my_addr, (socklen_t *)&addrlen)) < 0) {
+        if ((new_sd = accept(sd, (struct sockaddr *)&client_addr, (socklen_t *)&addrlen)) < 0) {
             cerr << "ERROR: accept() failed" << endl;
             return -1;
         }
-        //current_time = time(NULL);
-        //strcpy(timeBuf, ctime(&current_time));
-        //timeBuf[strlen(timeBuf)-1] = '\0';
 
         memset(buffer, 0, MAXBUF);
         read(new_sd, buffer, MAXBUF);
-        cout << "Buffer: " << buffer << endl;
 
-        if(strcmp(buffer, "#JOIN") == 0) {
-            cout << "\"" << getCurrentTime() << "\": Received a \"#JOIN\" action from agent " << inet_ntoa(my_addr.sin_addr) << "\n";
+        if (strcmp(buffer, "#JOIN") == 0) {
+            logStr = "\"" + getCurrentTime() + "\": Received a \"#JOIN\" action from agent \"" + inet_ntoa(client_addr.sin_addr) + "\"";
+            writeFile(logStr);
             response = "$OK";
             write(new_sd, response.c_str(), response.length());
+            logStr = "\"" + getCurrentTime() + "\": Responded to agent \"" + inet_ntoa(client_addr.sin_addr) + "\" with \"" + response + "\"";
+            writeFile(logStr);
         }
-        else if(strcmp(buffer, "#LEAVE") == 0) {
+        else if (strcmp(buffer, "#LEAVE") == 0) {
+            logStr = "\"" + getCurrentTime() + "\": Recieved a \"#LEAVE\" action from agent \"" + inet_ntoa(client_addr.sin_addr) + "\"";
+            writeFile(logStr);
             response = "$OK";
             write(new_sd, response.c_str(), response.length());
+            logStr = "\"" + getCurrentTime() + "\": Responded to agent \"" + inet_ntoa(client_addr.sin_addr) + "\" with \"" + response + "\"";
+            writeFile(logStr);
         }
-        else if(strcmp(buffer, "#LIST") == 0) {
-            response = "Returning list of active active agents...";
-            write(new_sd, response.c_str(), response.length());
+        else if (strcmp(buffer, "#LIST") == 0) {
+            logStr = "\"" + getCurrentTime() + "\": Recieved a \"#LIST\" action from agent \"" + inet_ntoa(client_addr.sin_addr) + "\"";
+            writeFile(logStr);
         }
-        else if(strcmp(buffer, "#LOG") == 0) {
-           response = "Returning a file called log.txt";
-           write(new_sd, response.c_str(), response.length());
+        else if (strcmp(buffer, "#LOG") == 0) {
+            logStr = "\"" + getCurrentTime() + "\": Recieved a \"#LOG\" action from agent \"" + inet_ntoa(client_addr.sin_addr) + "\"";
+            writeFile(logStr);
         }
 
         close(new_sd);
