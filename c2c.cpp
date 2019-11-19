bbe104600d7c9cebc18555c0e5c3a0e98d0bf0a0
Steven Hernandez Jr <s_h267@txstate.edu>
2019-11-18 22:00:12 -0600

    Finished conditional check/First step for logging

--- c2c.cpp
+++ c2c.cpp
@@ -6,26 +6,40 @@
  * ************************/
 
 // List of includes
-#include <iostream>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <iostream>
 #include <unistd.h>
 #include <string.h>
 #include <cstring>
 #include <cstdlib>
+#include <ctime>
 
 #define MAXBUF 1024
 
 using namespace std;
+
+string getCurrentTime() {
+   time_t current_time;
+   char timeBuf[256];
+   current_time = time(NULL);
+   strcpy(timeBuf, ctime(&current_time));
+   timeBuf[strlen(timeBuf)-1] = '\0';
+   string s(timeBuf);
+   return s;
+}
 
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         cout << "Usage: c2c server_port" << endl;
         return 0;
     }
+
+    time_t current_time;
+    char timeBuf[256];
 
     string sPort = argv[1];
     int nPort = atoi(sPort.c_str());
@@ -59,21 +73,37 @@ int main(int argc, char* argv[]) {
     }
 
     char buffer[MAXBUF];
+    string response;
 
     while(1) {
         if ((new_sd = accept(sd, (struct sockaddr *)&my_addr, (socklen_t *)&addrlen)) < 0) {
             cerr << "ERROR: accept() failed" << endl;
             return -1;
         }
+        //current_time = time(NULL);
+        //strcpy(timeBuf, ctime(&current_time));
+        //timeBuf[strlen(timeBuf)-1] = '\0';
 
         memset(buffer, 0, MAXBUF);
         read(new_sd, buffer, MAXBUF);
         cout << "Buffer: " << buffer << endl;
 
-        string response;
-        if(buffer == "#JOIN") {
+        if(strcmp(buffer, "#JOIN") == 0) {
+            cout << "\"" << getCurrentTime() << "\": Received a \"#JOIN\" action from agent " << inet_ntoa(my_addr.sin_addr) << "\n";
             response = "$OK";
             write(new_sd, response.c_str(), response.length());
+        }
+        else if(strcmp(buffer, "#LEAVE") == 0) {
+            response = "$OK";
+            write(new_sd, response.c_str(), response.length());
+        }
+        else if(strcmp(buffer, "#LIST") == 0) {
+            response = "Returning list of active active agents...";
+            write(new_sd, response.c_str(), response.length());
+        }
+        else if(strcmp(buffer, "#LOG") == 0) {
+           response = "Returning a file called log.txt";
+           write(new_sd, response.c_str(), response.length());
         }
 
         close(new_sd);
